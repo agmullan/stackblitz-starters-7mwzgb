080591ab13571af2c4ac63f3518fc6d8740cb58c
agmullan <33397452+agmullan@users.noreply.github.com>
2023-08-23 12:26:51 -0400

    3 files modified, 5 added, 4 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import * as React from 'react';
 import './style.css';
 import Card from './components/card.js'
 import Background from './components/background.js'
+import AboutSection from './components/aboutsection.js'
 
 export default function App() {
   return (
     <div>
-      <div style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/image.png'})` }}></div>
-      <Card content={[
+    <div>
+      <div className='banner'></div>
+    <Card content={[
         <div>
           <h1>MANICURES</h1>
           <p>BY ALLIE</p>
           <div className='row'>
+            <p/>
             <p className='subtitle'>Greater Boston • Independent Manicurist</p>
           </div>
         </div>
         ]}>
       </Card>
+    </div>
+    <AboutSection/>
     </div>
   );
 }
---
+++ src/components/aboutsection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import React, { Component } from 'react'
+
+const AboutSection = (props) => {
+  return (
+    <div className='row'>
+      <div><h1>about</h1></div>
+    
+    </div>
+  );
+};
+export default AboutSection;
